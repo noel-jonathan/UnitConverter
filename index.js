20a2ed9f1a7f7c1c276faf640aa4bc8d0ac5d7f0
kugel-qt <113015908+kugel-qt@users.noreply.github.com>
2022-09-11 23:30:20 +0200

    implemented conversion and render function
    used template strings and arguments to limit the number of functions as I learned in the last Figma course

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
-
 const input = document.querySelector("#input")
 const convertBtn = document.querySelector("#convert-btn")
 const lengthEl = document.querySelector("#length-el")
 const massEl = document.querySelector("#mass-el")
 const volumeEl = document.querySelector("#volume-el")
-
-convertBtn.addEventListener("click", {
-    // 
-})
-
-function renderConversion (typeEl, metric, imperial) {
-    input.value = 20
-    typeEl.textContent = `${input.value} ${metric} = 65.616 ${imperial} | ${input.value} ${imperial} = 6.096 ${metric}`
+const convertRatio = {
+    meters: 3.281,
+    liters: 0.264,
+    kilograms: 2.204
 }
 
-unitConversion (lengthEl, "meters", "feets")
-unitConversion (massEl, "kilograms", "pounds")
-unitConversion (volumeEl, "liters", "gallons")
+input.value = 10
+
+convertBtn.addEventListener ("click", function() {
+    render()
+})
+
+input.addEventListener ("keyup", function() {
+    render()
+})
+
+function convert(value, ratio, conversionWay) {
+    if (conversionWay) {
+        return value * ratio
+    }
+    return value / ratio
+}
+
+function renderConversion(targetEl, metric, imperial) {
+    targetEl.textContent = `
+        ${input.value} ${metric} = 
+        ${convert(input.value, convertRatio[metric], true).toFixed(2)} ${imperial} | 
+        ${input.value} ${imperial} =
+        ${convert(input.value, convertRatio[metric], false).toFixed(2)} ${metric}
+    `
+}
+
+function render() {
+    renderConversion (lengthEl, "meters", "feet")
+    renderConversion (volumeEl, "liters", "gallons")
+    renderConversion (massEl, "kilograms", "pounds")
+}
+
+render()
